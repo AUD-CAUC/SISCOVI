78b414200cbe200f8735718649a21e64e0725e52
Lucas Penido <lpenidoa@me.com>
2019-05-13 11:03:41 -0300

    Arredondando valores Saldo Total Individual
    Com o arredondamento evita-se que apareça -0 no Saldo Individual.

--- Back-end/src/main/java/br/jus/stj/siscovi/calculos/Saldo.java
+++ Back-end/src/main/java/br/jus/stj/siscovi/calculos/Saldo.java
@@ -401,12 +401,12 @@ public class Saldo {
 
             try {
 
-                preparedStatement = connection.prepareStatement("SELECT SUM(tmr.ferias + CASE WHEN rtm.ferias IS NULL THEN 0 ELSE rtm.ferias END) AS \"Férias retido\",\n" +
-                        "           SUM(tmr.terco_constitucional + CASE WHEN rtm.terco_constitucional IS NULL THEN 0 ELSE rtm.terco_constitucional END)  AS \"Abono de férias retido\",\n" +
-                        "           SUM(tmr.decimo_terceiro + CASE WHEN rtm.decimo_terceiro IS NULL THEN 0 ELSE rtm.decimo_terceiro END) AS \"Décimo terceiro retido\",\n" +
-                        "           SUM(tmr.incidencia_submodulo_4_1 + CASE WHEN rtm.incidencia_submodulo_4_1 IS NULL THEN 0 ELSE rtm.incidencia_submodulo_4_1 END) AS \"Incid. do submód. 4.1 retido\",\n" +
-                        "           SUM(tmr.multa_fgts + CASE WHEN rtm.multa_fgts IS NULL THEN 0 ELSE rtm.multa_fgts END) AS \"Multa do FGTS retido\"," +
-                        "           SUM(tmr.total + CASE WHEN rtm.total IS NULL THEN 0 ELSE rtm.total END) AS \"Total retido\"\n" +
+                preparedStatement = connection.prepareStatement("SELECT ROUND(SUM(tmr.ferias + CASE WHEN rtm.ferias IS NULL THEN 0 ELSE rtm.ferias END), 2) AS \"Férias retido\",\n" +
+                        "           ROUND(SUM(tmr.terco_constitucional + CASE WHEN rtm.terco_constitucional IS NULL THEN 0 ELSE rtm.terco_constitucional END), 2)  AS \"Abono de férias retido\",\n" +
+                        "           ROUND(SUM(tmr.decimo_terceiro + CASE WHEN rtm.decimo_terceiro IS NULL THEN 0 ELSE rtm.decimo_terceiro END), 2) AS \"Décimo terceiro retido\",\n" +
+                        "           ROUND(SUM(tmr.incidencia_submodulo_4_1 + CASE WHEN rtm.incidencia_submodulo_4_1 IS NULL THEN 0 ELSE rtm.incidencia_submodulo_4_1 END), 2) AS \"Incid. do submód. 4.1 retido\",\n" +
+                        "           ROUND(SUM(tmr.multa_fgts + CASE WHEN rtm.multa_fgts IS NULL THEN 0 ELSE rtm.multa_fgts END), 2) AS \"Multa do FGTS retido\"," +
+                        "           ROUND(SUM(tmr.total + CASE WHEN rtm.total IS NULL THEN 0 ELSE rtm.total END), 2) AS \"Total retido\"\n" +
                         "      FROM tb_total_mensal_a_reter tmr\n" +
                         "        JOIN tb_terceirizado_contrato tc ON tc.cod = tmr.cod_terceirizado_contrato\n" +
                         "        LEFT JOIN tb_retroatividade_total_mensal rtm ON rtm.cod_total_mensal_a_reter = tmr.cod\n" +
